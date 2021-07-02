50494c0c109fba183e5f58bdd7abf2b44d49fe81
Gopa11313 <gopalaa9819525993@gmail.com>
2021-07-02 12:05:56 +0545

    seller repository

---
+++ app/src/main/java/com/example/medicinefinder/repository/SellerRepository.kt
@@ -0,0 +1,16 @@
+package com.example.medicinefinder.repository
+
+import com.example.medicinefinder.api.MyApiRequest
+import com.example.medicinefinder.api.SellerApi
+import com.example.medicinefinder.api.ServiceBuilder
+import com.example.medicinefinder.model.Seller
+import com.example.medicinefinder.response.SellerResponse
+
+class SellerRepository:MyApiRequest() {
+    val myapi= ServiceBuilder.buildServices(SellerApi::class.java)
+    suspend fun SignupUser(seller:Seller):SellerResponse{
+        return  apiRequest {
+        myapi.UserSignup(seller)
+        }
+    }
+}
