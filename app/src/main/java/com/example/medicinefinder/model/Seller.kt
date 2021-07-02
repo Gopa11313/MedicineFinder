cb27d1aefec952901c2dba123e9ba70f34f140d1
Gopa11313 <gopalaa9819525993@gmail.com>
2021-07-02 12:05:25 +0545

    seller model

---
+++ app/src/main/java/com/example/medicinefinder/model/Seller.kt
@@ -0,0 +1,12 @@
+package com.example.medicinefinder.model
+
+data class Seller (
+    var name:String?=null,
+    var email:String?=null,
+    var password:String?=null,
+    var image:String?=null,
+    var role:String?=null,
+    var latitude:String?=null,
+    var longitude:String?=null
+        ){
+}
