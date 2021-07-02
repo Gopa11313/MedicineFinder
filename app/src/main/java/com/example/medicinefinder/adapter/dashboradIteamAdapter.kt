2a8a86380e8d693e744bb8ea90d37d40838a5cf9
Gopa11313 <gopalaa9819525993@gmail.com>
2021-07-02 12:51:25 +0545

    adapter to bind data in dashboard itam fragmment

---
+++ app/src/main/java/com/example/medicinefinder/adapter/dashboradIteamAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.medicinefinder.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.medicinefinder.R
+import com.example.medicinefinder.model.DashBoradIteam
+
+class dashboradIteamAdapter (private val context: Context, private val arrayList: java.util.ArrayList<DashBoradIteam>) : BaseAdapter() {
+
+    private lateinit var title: TextView
+    private lateinit var icon: ImageView
+    override fun getCount(): Int {
+        return arrayList.size
+    }
+    override fun getItem(position: Int): Any {
+        return position
+    }
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
+        var convertView = convertView
+        convertView = LayoutInflater.from(context).inflate(R.layout.dashboarditeam, parent, false)
+        title = convertView.findViewById(R.id.dsh_title)
+        icon = convertView.findViewById(R.id.dsh_icon)
+        title.text = arrayList[position].title
+        icon.setImageResource(arrayList[position].icon)
+        return convertView
+    }
+}
