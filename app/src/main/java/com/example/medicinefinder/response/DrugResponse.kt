0a2db8f9ddbc83b95674873b118d5ded2abb5a66
Gopa11313 <gopalaa9819525993@gmail.com>
2021-07-05 16:29:14 +0545

    drug ressponse

---
+++ app/src/main/java/com/example/medicinefinder/response/DrugResponse.kt
@@ -0,0 +1,10 @@
+package com.example.medicinefinder.response
+
+import com.example.medicinefinder.model.Drug
+
+data class DrugResponse(
+    var success:Boolean?=null,
+    var msg:String?=null,
+    var data:List<Drug>?=null
+) {
+}
