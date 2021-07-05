0b5693425d14a1ca81624b6b2f6c3ddbddee5679
Gopa11313 <gopalaa9819525993@gmail.com>
2021-07-05 15:50:32 +0545

    binding uploaddrugfragement view

--- app/src/main/java/com/example/medicinefinder/ui/ui/uploaddrug/UploadDrugFragment.kt
+++ app/src/main/java/com/example/medicinefinder/ui/ui/uploaddrug/UploadDrugFragment.kt
@@ -1,18 +1,23 @@
-package com.example.medicinefinder.ui.ui.gallery
+package com.example.medicinefinder.ui.ui.uploaddrug
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.medicinefinder.R
 import com.example.medicinefinder.databinding.FragmentGalleryBinding
 
-class GalleryFragment : Fragment() {
-
-    private lateinit var galleryViewModel: GalleryViewModel
+class UploadDrugFragment : Fragment() {
+private lateinit var medicine_name:EditText
+private lateinit var prescription:EditText
+private lateinit var upload:Button
+    private lateinit var uploadDrugViewModel: UploadDrugViewModel
     private var _binding: FragmentGalleryBinding? = null
 
     // This property is only valid between onCreateView and
@@ -24,21 +29,29 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        uploadDrugViewModel =
+            ViewModelProvider(this).get(UploadDrugViewModel::class.java)
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        medicine_name=view.findViewById(R.id.medicine_name)
+        prescription=view.findViewById(R.id.prescription)
+        upload=view.findViewById(R.id.upload)
+        upload.setOnClickListener(){
+
+        }
+    }
+    fun uploadMedicine(){
+
     }
 }
